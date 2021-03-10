e750d7f67ca00e5caa14b69ebc57ecf473add4ca
LakunCindy <43467524+LakunCindy@users.noreply.github.com>
2021-03-10 18:36:17 +0100

    Add files via upload

---
+++ mySearchEngine/myManageSale/management/command/refreshId.py
@@ -0,0 +1,21 @@
+from django.core.management.base import BaseCommand, CommandError
+from myManageSale.models import ProductSale
+from myManageSale.serializers import ProductSaleSerializer
+from myManageSale.config import baseUrl
+import requests
+import time
+
+class Command(BaseCommand):
+    help = 'Refresh the list of products which are on sale.'
+
+    def handle(self, *args, **options):
+        self.stdout.write('['+time.ctime()+'] Refreshing data...')
+        response = requests.get(baseUrl+'products/')
+        jsondata = response.json()
+        ProductSale.objects.all().delete()
+        for product in jsondata:
+                serializer = ProductSaleSerializer(data={'tigID':str(product['id'])})
+                if serializer.is_valid():
+                    serializer.save()
+                    self.stdout.write(self.style.SUCCESS('['+time.ctime()+'] Successfully added product id="%s"' % product['id']))
+        self.stdout.write('['+time.ctime()+'] Data refresh terminated.')
